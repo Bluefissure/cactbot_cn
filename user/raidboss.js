caf2cdc4346c6b5d3e58f652f36b604e75be3a09
Bluefissure <bluefissure@foxmail.com>
2019-06-19 23:29:14 +0800

    fix timeline

--- user/raidboss.js
+++ user/raidboss.js
@@ -8,3 +8,69 @@
 // log window in ACT.
 
 Options.Language = 'cn';
+
+
+// If false, no timeline of upcoming events will be displayed during fights.
+Options.TimelineEnabled = true;
+
+// If false, triggers and timelines will not show or speak text, nor play
+// sounds.
+Options.AlertsEnabled = true;
+
+// If false, then visual text alerts are not shown for triggers.
+Options.TextAlertsEnabled = true;
+
+// If false, then sound alerts are not played.
+Options.SoundAlertsEnabled = true;
+
+// If true, then text-to-speech alerts are read aloud.  Text-to-speech takes
+// priority over custom sounds and text noises.  If a trigger does not have
+// a tts entry then it will fall back on text and sound (if turned on).
+// TTS 开启与否
+Options.SpokenAlertsEnabled = false;
+
+// Will override the singular TTS alerts if a group alert is set for a specific trigger
+// Change phrasing to make sense in a group setting
+Options.GroupSpokenAlertsEnabled = false;
+
+
+// Show timer bars for events that will happen in this many seconds or less.
+Options.ShowTimerBarsAtSeconds = 30;
+
+// Once a timer bar reaches 0, keep it around this long after.
+Options.KeepExpiredTimerBarsForSeconds = 0.7;
+
+// Change the bar color to highlight it is coming up when this many seconds
+// are left on it.
+Options.BarExpiresSoonSeconds = 8;
+
+// Number of bars to show in the space given to the UI by css.
+Options.MaxNumberOfTimerBars = 6;
+
+
+// Path to sound played for info-priority text popups, or when "Info" is
+// specified as the sound name.
+Options.InfoSound = '../../resources/sounds/freesound/percussion_hit.ogg';
+
+// Path to sound played for alert-priority text popups, or when "Alert" is
+// specified as the sound name.
+Options.AlertSound = '../../resources/sounds/BigWigs/Alert.ogg';
+
+// Path to sound played for alarm-priority text popups, or when "Alarm" is
+// specified as the sound name.
+Options.AlarmSound = '../../resources/sounds/BigWigs/Alarm.ogg';
+
+// Path to sound played when "Long" is specified as the sound name.
+Options.LongSound = '../../resources/sounds/BigWigs/Long.ogg';
+
+// Volume between 0 and 1 to play the InfoSound at.
+Options.InfoSoundVolume = 1;
+
+// Volume between 0 and 1 to play the AlertSound at.
+Options.AlertSoundVolume = 1;
+
+// Volume between 0 and 1 to play the AlarmSound at.
+Options.AlarmSoundVolume = 1;
+
+// Volume between 0 and 1 to play the LongSound at.
+Options.LongSoundVolume = 1;
