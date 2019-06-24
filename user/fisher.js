5c8a29844689f6c87fd7b6a9bde6d69e4607dca4
Bluefissure <bluefissure@foxmail.com>
2019-06-23 23:56:21 -0400

    test fish

---
+++ user/fisher.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Rename this file to `raidboss.js` and edit it to change the raidboss ui.
+// This file is Javascript.  Anything after "//" on a line is a comment.
+// If you edit this file, remember to reload ACT or click the "Reload overlay"
+// button on the raidboss CactbotOverlay.
+// If there are errors in this file, they will appear in the OverlayPlugin.dll
+// log window in ACT.
+
+Options.Language = 'cn';
